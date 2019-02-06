5178f96938683cf4d1f58c7956c73d13f25e1412
Mauricio <Mauricio@192.168.100.71>
2019-02-06 15:24:20 -0600

    segunda version

--- proyecto_repo/src/proyecto_repo/Proyecto_repo.java
+++ proyecto_repo/src/proyecto_repo/Proyecto_repo.java
@@ -15,7 +15,7 @@ public class Proyecto_repo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("prueba repositorio");
     }
     
 }
